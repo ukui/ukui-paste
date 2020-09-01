c7b3b8b83484f708cbf978ad708f7717922226bc
Jackie Liu <liuyun01@kylinos.cn>
2020-09-01 13:36:55 +0800

    Add highdpi support for X11 Linux
    Signed-off-by: Jackie Liu <liuyun01@kylinos.cn>

--- main.cpp
+++ main.cpp
@@ -1,14 +1,45 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QGuiApplication>
 #include <SingleApplication>
 
 #ifdef Q_OS_LINUX
 #include <KF5/KWindowSystem/KWindowEffects>
+#include <X11/Xlib.h>
+
+static int getScreenWidth(void)
+{
+	Display *display;
+	Screen *screen;
+	int width = 0;
+
+	display = XOpenDisplay(NULL);
+	if (!display)
+		return 0;
+
+	screen = DefaultScreenOfDisplay(display);
+	if (!screen)
+		goto out;
+
+	width = screen->width;
+out:
+	XCloseDisplay(display);
+
+	return width;
+}
+
 #endif
 
 int main(int argc, char *argv[])
 {
+#ifdef Q_OS_LINUX
+	if (getScreenWidth() > 2560) {
+		QGuiApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+		QGuiApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
+	}
+#endif
+
 	SingleApplication a(argc, argv);
 
 	MainWindow w;
